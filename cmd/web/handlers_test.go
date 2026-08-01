98f9d342d4e4f0d2ae2e5e15e716702b50988a68
dekutyavin <daniil.kutyavin94@gmail.com>
2026-08-01 21:56:03 +0300

    test: testutils file

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,41 +1,18 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log"
-	"log/slog"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"snippetbox.dekutyavin.net/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		logger: slog.New(slog.DiscardHandler),
-	}
-	ts := httptest.NewTLSServer(app.routes())
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	req, err := http.NewRequest(http.MethodGet, ts.URL+"/ping", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	res, err := ts.Client().Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-
-	assert.Equal(t, res.StatusCode, http.StatusOK)
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	body = bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
+	res := ts.get(t, "/ping")
+	assert.Equal(t, res.status, http.StatusOK)
+	assert.Equal(t, res.body, "OK")
 }
---
+++ cmd/web/testutils_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func newTestApplication(t *testing.T) *application {
+	return &application{
+		logger: slog.New(slog.DiscardHandler),
+	}
+}
+
+type testServer struct {
+	*httptest.Server
+}
+
+func newTestServer(t *testing.T, h http.Handler) *testServer {
+	ts := httptest.NewTLSServer(h)
+	return &testServer{ts}
+}
+
+type testResponse struct {
+	status  int
+	headers http.Header
+	cookies []*http.Cookie
+	body    string
+}
+
+func (ts *testServer) get(t *testing.T, urlPath string) testResponse {
+	req, err := http.NewRequest(http.MethodGet, ts.URL+urlPath, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := ts.Client().Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return testResponse{
+		status:  res.StatusCode,
+		headers: res.Header,
+		cookies: res.Cookies(),
+		body:    string(bytes.TrimSpace(body)),
+	}
+}
