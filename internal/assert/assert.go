e0cf6b5a9cd2206f59645cbed6127a846f359f09
dekutyavin <daniil.kutyavin94@gmail.com>
2026-08-01 12:44:14 +0300

    test: use better comparison for assert.Equal; add assert.Nil, assert.True

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,11 +1,52 @@
 package assert
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Equal[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 
-	if got != want {
+	if !isEqual(got, want) {
 		t.Errorf("got: %v, want: %v", got, want)
 	}
+}
+
+func True(t *testing.T, got bool) {
+	t.Helper()
+
+	if !got {
+		t.Errorf("got: %v, want: true", got)
+	}
+}
+
+func Nil(t *testing.T, got any) {
+	t.Helper()
+
+	if !isNil(got) {
+		t.Errorf("got: %v, want: nil", got)
+	}
+}
+
+func isEqual[T any](got, want T) bool {
+	if isNil(got) && isNil(want) {
+		return true
+	}
+
+	return reflect.DeepEqual(got, want)
+}
+
+func isNil(v any) bool {
+	if v == nil {
+		return true
+	}
+
+	rv := reflect.ValueOf(v)
+	switch rv.Kind() {
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Pointer, reflect.Slice, reflect.UnsafePointer:
+		return rv.IsNil()
+	}
+
+	return false
 }
